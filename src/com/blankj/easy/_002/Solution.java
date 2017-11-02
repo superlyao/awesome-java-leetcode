ddf3fb917693cb9bba6a1f88b51bef1b80f1e9f4
yliao <superyao12@sina.com>
2017-11-02 21:32:31 +0800

    数组查找

---
+++ src/com/blankj/easy/_002/Solution.java
@@ -0,0 +1,21 @@
+package com.blankj.easy._002;
+
+/**
+ * Created by liaoyao on 2017/11/2.
+ */
+public class Solution {
+    public boolean Find(int target,int [][] array) {
+        int len = array.length-1;
+        int i = 0;
+        while((len >= 0)&& (i < array[0].length)){
+            if(array[len][i] > target){
+                len--;
+            }else if(array[len][i] < target){
+                i++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
